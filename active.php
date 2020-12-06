d71266c0d1030c0e274698e386e05a14055b3fcd
Dogmink <braulio25sc@gmail.com>
2020-12-06 17:18:38 -0500

    Se modificó active, y se cambió el icono de la página.

--- active.php
+++ active.php
@@ -9,27 +9,28 @@ if (isset($_GET['username']) && isset($_GET['hash'])) {
     $hash = $_GET['hash'];
     $username = $_GET['username'];
     $user->validateActiveAccount($username, $hash);
-        ?>
-        <!DOCTYPE html>
-        <html lang="en" dir="ltr">
-        <head>
-          <meta charset="utf-8">
-          <title>BlackEdge Store | Activación de cuenta</title>
-          <link rel="stylesheet" href="/css/grid.css">
-          <link rel="stylesheet" href="/css/estilos.css">
-        </head>
-        <body>
-          <div class="contenido">
-            <h1 style="text-align: center;padding-top: 120px;padding-bottom: 40px;">Todo listo, <b style="color: var(--hovercolor1);">A</b></h1>
-            <h3 style="text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en unos momentos.</h3>
-            <script type="text/javascript">
-            setTimeout(function () {
-              window.location.href = "index.php";
-            }, 3000);
-            </script>
-          </div>
-        </body>
-        </html>
-        <?php
-      }
+  } else {
+    ?>
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>BlackEdge Store | Activación de cuenta</title>
+      <link rel="stylesheet" href="/css/grid.css">
+      <link rel="stylesheet" href="/css/estilos.css">
+    </head>
+    <body>
+      <div class="contenido">
+        <h1 style="font-size: 50px; text-align: center;padding-top: 120px;padding-bottom: 40px;">Oops, algo salió <b style="color: var(--hovercolor1);">mal</b></h1>
+        <h3 style="font-size: 25px; text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en uno momento.</h3>
+        <script type="text/javascript">
+        setTimeout(function () {
+          window.location.href = "index.php";
+        }, 6000);
+        </script>
+      </div>
+    </body>
+    </html>
+    <?php
+  }
  ?>
--- resources/user.php
+++ resources/user.php
@@ -58,12 +58,35 @@ class User
       </head>
       <body>
         <div class="contenido">
-          <h1 style="text-align: center;padding-top: 120px;padding-bottom: 40px;">Todo listo, <b style="color: var(--hovercolor1);">A</b></h1>
-          <h3 style="text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en unos momentos.</h3>
+          <h1 style="font-size: 50px; text-align: center;padding-top: 120px;padding-bottom: 40px;">Todo listo, <b style="color: var(--hovercolor1);"><?php print $username ?></b></h1>
+          <h3 style="font-size: 25px; text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en uno momento.</h3>
           <script type="text/javascript">
           setTimeout(function () {
             window.location.href = "index.php";
-          }, 3000);
+          }, 6000);
+          </script>
+        </div>
+      </body>
+      </html>
+      <?php
+    } else {
+      ?>
+      <!DOCTYPE html>
+      <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>BlackEdge Store | Activación de cuenta</title>
+        <link rel="stylesheet" href="/css/grid.css">
+        <link rel="stylesheet" href="/css/estilos.css">
+      </head>
+      <body>
+        <div class="contenido">
+          <h1 style="font-size: 50px; text-align: center;padding-top: 120px;padding-bottom: 40px;">Oops, algo salió <b style="color: var(--hovercolor1);">mal</b></h1>
+          <h3 style="font-size: 25px; text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en uno momento.</h3>
+          <script type="text/javascript">
+          setTimeout(function () {
+            window.location.href = "index.php";
+          }, 6000);
           </script>
         </div>
       </body>
