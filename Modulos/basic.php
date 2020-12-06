3559b32e815acaed5a5f8ae08cc25601fc06836b
Dogmink <braulio25sc@gmail.com>
2020-12-06 17:19:58 -0500

    Se modificó el icono de la página.

--- Modulos/basic.php
+++ Modulos/basic.php
@@ -11,7 +11,7 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link class='iconweb' rel="icon" type="image/png" href="/images/icon/shop.png" />
+  <link class='iconweb' rel="icon" type="image/png" href="/images/icon/IconWeb.png" />
   <link rel="stylesheet" href="css/normalize.css">
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet">
