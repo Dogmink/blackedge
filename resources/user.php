e4469985963f003e129c31b6645ad560e7872d12
Dogmink <braulio25sc@gmail.com>
2020-12-06 04:59:41 -0500

    Corrigiendo posible error.

--- resources/user.php
+++ resources/user.php
@@ -37,32 +37,74 @@ class User
 
 
 
-    function errorLogin($logErr){
+  function errorLogin($logErr){
         $logErr = "Las credenciales son incorrectas";
         print $logErr;
     }
 
 
   function userRegister($_params){
-      $sql = "INSERT INTO user (username, password, email) VALUES (:username, SHA(:password), :email)";
+      $sql = "INSERT INTO user (username, password, email, hash) VALUES (:username, SHA(:password), :email, :hash)";
 
       $result = $this->cn->prepare($sql);
 
+      $hash = md5(rand(0, 1000));
 
       $_array = array(
         ":username" => $_params['username'],
         ":password" => $_params['password'],
         ":email" => $_params['email'],
+        ":hash" => $hash;
       );
 
       if($result->execute($_array)){
+        $this->sendEmail();
         ?>
         <script type="text/javascript">
           window.location= '../login.php';
         </script>
         <?php
+        die();
       }
     }
+
+
+  function sendEmail(){
+    $userlog =  $_SESSION['user_log'];
+    $username = $userlog['username'];
+    $email = $userlog['email'];
+    $hash = $userlog['hash'];
+    $sql = "SELECT COUNT(username) as focususer FROM user WHERE email = :email AND active = 0";
+    $stmt = $this->cn->prepare($sql);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+    if ($row['focususer']==1) {
+      $to = $email;
+      $subject = "BlackEdge | Activación de cuenta.";
+      $message ="
+      <!DOCTYPE html>
+      <html lang="en" dir="ltr">
+        <head>
+          <meta charset="utf-8">
+          <title>BlackEdge Activation Account</title>
+        </head>
+        <body>
+          <h1>Gracias por registrarte en BlackEdge Store ".$username."</h1>
+          <p>Activa tu <b>cuenta</b> con este enlace:</p><br>
+          <a href="blackedge.com/activeacount.php?email='.$email.'&hash='.$hash.'"></a>
+          <p>No compartas tu credenciales con nadie.</p>
+          <img src="blackedge.com/images/Logo/Logo.png" alt="">
+        </body>
+      </html>";
+      $headers = 'From:noreply@blackedgestore.com . "\r\n"';
+      mail($to, $subject, $message, $headers);
+      die();
+    }
+  }
+
+
+
   function validateUsername($username){
     $sql = "SELECT COUNT(username) as num FROM user WHERE username = :username";
     $stmt = $this->cn->prepare($sql);
