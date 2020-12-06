b9785be6a36b3eadf924b33c1a087ed29a2ee40a
Dogmink <braulio25sc@gmail.com>
2020-12-06 13:18:15 -0500

    se cambio el else, para descartar unos errores.

--- active.php
+++ active.php
@@ -1,5 +1,4 @@
 <?php
-include 'resources/user.php';
 $user = new BlackEdgeStore\User;
 
 $hash = '';
@@ -32,29 +31,29 @@ if (isset($_GET['$hash']) && isset($_GET['$email'])) {
         </body>
         </html>
         <?php
-      } else {
-        ?>
-        <!DOCTYPE html>
-        <html lang="en" dir="ltr">
-        <head>
-          <meta charset="utf-8">
-          <title>BlackEdge Store | Activación de cuenta</title>
-          <link rel="stylesheet" href="/css/grid.css">
-          <link rel="stylesheet" href="/css/estilos.css">
-        </head>
-        <body>
-          <div class="contenido">
-            <h1 style="text-align: center;padding-top: 120px;padding-bottom: 40px;">Oops, algo salió <b style="color: var(--hovercolor1);">mal</b></h1>
-            <h3 style="text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en unos momentos.</h3>
-            <script type="text/javascript">
-            setTimeout(function () {
-              window.location.href = "index.php";
-            }, 3000);
-            </script>
-          </div>
-        </body>
-        </html>
-        <?php
       }
+  }else {
+    ?>
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>BlackEdge Store | Activación de cuenta</title>
+      <link rel="stylesheet" href="/css/grid.css">
+      <link rel="stylesheet" href="/css/estilos.css">
+    </head>
+    <body>
+      <div class="contenido">
+        <h1 style="text-align: center;padding-top: 120px;padding-bottom: 40px;">Oops, algo salió <b style="color: var(--hovercolor1);">mal</b></h1>
+        <h3 style="text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en unos momentos.</h3>
+        <script type="text/javascript">
+        setTimeout(function () {
+          window.location.href = "index.php";
+        }, 3000);
+        </script>
+      </div>
+    </body>
+    </html>
+    <?php
   }
  ?>
