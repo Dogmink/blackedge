e3aad442a404b3b2ff50811a4bbfcc9b5506f8c9
Dogmink <braulio25sc@gmail.com>
2020-12-06 13:43:05 -0500

    test5

--- active.php
+++ active.php
@@ -32,28 +32,4 @@ if (isset($_GET['email']) && isset($_GET['hash'])) {
         </html>
         <?php
       }
-  }else {
-    ?>
-    <!DOCTYPE html>
-    <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title>BlackEdge Store | Activación de cuenta</title>
-      <link rel="stylesheet" href="/css/grid.css">
-      <link rel="stylesheet" href="/css/estilos.css">
-    </head>
-    <body>
-      <div class="contenido">
-        <h1 style="text-align: center;padding-top: 120px;padding-bottom: 40px;">Oops, algo salió <b style="color: var(--hovercolor1);">mal</b></h1>
-        <h3 style="text-align: center;padding-top: 50px;padding-bottom: 40px;">Se te redireccionará en unos momentos.</h3>
-        <script type="text/javascript">
-        setTimeout(function () {
-          window.location.href = "index.php";
-        }, 3000);
-        </script>
-      </div>
-    </body>
-    </html>
-    <?php
-  }
  ?>
