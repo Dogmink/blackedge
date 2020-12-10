816eee2afb99d7a2fa11c38117620dff7ae0853d
Dogmink <braulio25sc@gmail.com>
2020-12-10 07:20:06 -0500

    test 2

--- panel/loginactions.php
+++ panel/loginactions.php
@@ -74,14 +74,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
   if ($_POST['accion']==='GUARDAR'){
-    $_params = array(
-      'nombres' => $_POST['nombres'],
-      'apellidos' => $_POST['apellidos'],
-      'dni' => $_POST['dni'],
-      'telf' => $_POST['telf'],
-      'direc' => $_POST['direc']
-    );
-    $user->updateInfo($_params);
+    
       }
     }
 
