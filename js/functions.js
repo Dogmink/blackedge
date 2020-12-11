08623bbd6837a3e3b626e887cf9c42bf58c327b5
Dogmink <braulio25sc@gmail.com>
2020-12-11 09:10:19 -0500

    prueba functions.js con register.php

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,21 @@
-function register() {
-  let formulario = document.getElementById('form-register');
-  let errorlog = document.getElementById('error-log-text');
+let formulario = document.getElementById('form-register');
+let errorlog = document.getElementById('error-log-text');
 
-  formulario.addEventListener('submit', function(e) {
-    e.preventDefault();
-    let datos = new FormData(formulario);
+formulario.addEventListener('submit', function(e) {
+  e.preventDefault();
+  let datos = new FormData(formulario);
 
-    let dUsername = datos.get('username');
-    let dPasssword = datos.get('password');
-    let dCPassword = datos.get('password-confirm');
+  let dUsername = datos.get('username');
+  let dPasssword = datos.get('password');
+  let dCPassword = datos.get('password-confirm');
 
-    if (dPasssword == dCPassword) {
-      fetch('../panel/loginactions.php', {
-        method: 'POST',
-        body: datos
-      });
-    } else {
-      errorlog.innerHTML = 'Las <b>contraseñas</b> no coinciden.';
-    }
-  });
+  if (dPasssword == dCPassword) {
+    fetch('../panel/loginactions.php', {
+      method: 'POST',
+      body: datos
+    });
+  } else {
+    errorlog.innerHTML = 'Las <b>contraseñas</b> no coinciden.';
+  }
+});
 }
