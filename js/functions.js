72f51e0c1e9e19f1fead073aab39a0e4e1d7eed9
Dogmink <braulio25sc@gmail.com>
2020-12-11 09:58:25 -0500

    Se restauró hasta el punto en el que funcionaba.

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,21 @@
-let formulario = document.getElementById('formRegister');
-let errorlog = document.getElementById('error-log-text');
+function register() {
+  let formulario = document.getElementById('form-register');
 
-formulario.addEventListener('submit', function(e) {
-  e.preventDefault();
-  let datos = new FormData(formulario);
+  formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let datos = new FormData(formulario);
 
-  let dUsername = datos.get('username');
-  let dPasssword = datos.get('password');
-  let dCPassword = datos.get('password-confirm');
+    let dUsername = datos.get('username');
+    let dPasssword = datos.get('password');
+    let dCPassword = datos.get('password-confirm');
 
-  if (dPasssword == dCPassword) {
-    fetch('../panel/loginactions.php', {
-      method: 'POST',
-      body: datos
-    });
-  } else {
-    errorlog.innerHTML = 'Las <b>contraseñas</b> no coinciden.';
-  }
-});
-
-function userRegister(){
-
+    if (dPasssword == dCPassword) {
+      fetch('../panel/loginactions.php', {
+        method: 'POST',
+        body: datos
+      });
+    } else {
+      document.write('<p class = "error-login">Las contraseñas no coinciden</p>');
+    }
+  });
 }
