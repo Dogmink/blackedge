67bc1de0e25cae170ff29154b64b85f97d0c3e8f
Dogmink <braulio25sc@gmail.com>
2020-12-11 10:43:33 -0500

    solucionado, probando functions.php

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,22 @@
-var formulario = document.getElementById('formulario');
-var textError = document.getElementById('logErrorText');
+let formulario = document.getElementById('formulario');
+let textError = document.getElementById('logErrorText');
 
 if (formulario) {
-  formulario.addEventListener('submit', function(e){
+  formulario.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log('me diste un clic.');
+    let datos = new FormData(formulario);
+
+    let dUsername = datos.get('username');
+    let dPasssword = datos.get('password');
+    let dCPassword = datos.get('password-confirm');
+
+    if (dPasssword == dCPassword) {
+      fetch('../panel/loginactions.php', {
+        method: 'POST',
+        body: datos
+      });
+    } else {
+      textError.innerHTML('Las <b>contraseñas</b> no coinciden.')
+    }
   });
-}else{
-  console.log('No se encontró formulario.');
 }
