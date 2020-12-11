33c7f68d3ca4ab1275cb382f2308ef69d3fa6671
Dogmink <braulio25sc@gmail.com>
2020-12-11 10:11:09 -0500

    prueba

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
-  let formulario = document.getElementById('formRegister');
-  let textError = document.getElementById('logErrorText');
+formulario = document.getElementById('formRegister');
+textError = document.getElementById('logErrorText');
 
-  formulario.addEventListener('submit', function(e) {
-    e.preventDefault();
-    console.log('me diste un clic.')
-  });
+formulario.addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log('me diste un clic.')
+});
