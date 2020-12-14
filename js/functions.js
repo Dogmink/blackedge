4719e7ee2ac2c65697813448947452a2162f5129
Dogmink <braulio25sc@gmail.com>
2020-12-14 16:17:54 -0500

    Prueba sdasda

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,6 @@ let formulario = document.getElementById('formulario');
   formulario.addEventListener('submit', function(e) {
     e.preventDefault();
      let datos = new FormData(formulario);
-     console.log(datos.username);
-     console.log(datos.email);
+     console.log(datos.get('username'));
+     console.log(datos.get('email'));
 });
