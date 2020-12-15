ee15c24e673e2bfaac82fec7ac6a05f7f55e4a2e
Dogmink <braulio25sc@gmail.com>
2020-12-15 09:19:44 -0500

    arreglando posibles errores en functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,15 @@
 let formulario = document.getElementById('formulario');
 
-  formulario.addEventListener('submit', function(e) {
-    e.preventDefault();
-     let datos = new FormData(formulario);
+formulario.addEventListener('submit', function(e) {
+  e.preventDefault();
+  let datos = new FormData(formulario);
 
-     fetch('panel/loginactions.php',{
-       method: 'POST',
-       body: datos
-     });
-     .then( res => res.json());
-     .then( data => {
-       console.log(data);
-     });
+  fetch('panel/loginactions.php', {
+      method: 'POST',
+      body: datos
+    })
+    .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
 });
