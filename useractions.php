33d5b35f776d14b034eba3eadf5a7e400aba5b07
Dogmink <braulio25sc@gmail.com>
2020-12-15 19:10:36 -0500

    Tratando de solucionar problemas con js.

--- useractions.php
+++ useractions.php
@@ -1,9 +1,10 @@
 <?php
-  include 'resources/user.php';
-  $user = new BlackEdgeStore\User;
+  
   $accion = $_POST['accion'];
 
   if ($accion == "registro") {
+    include 'resources/user.php';
+    $user = new BlackEdgeStore\User;
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
