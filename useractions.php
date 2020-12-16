2ce772554de309a5c21a2981a9db8a113f2744c4
Dogmink <braulio25sc@gmail.com>
2020-12-15 19:36:33 -0500

    Prueba 3

--- useractions.php
+++ useractions.php
@@ -3,6 +3,20 @@
   $user = new BlackEdgeStore\User;
   
   $accion = $_POST['accion'];
-
-  echo json_encode($accion);
+  
+  if($accion == 'registro'){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cPassword = $_POST['password-confirm'];
+    
+    $parametros = array(
+      'username'=>$_POST['username'],
+      'password'=>$_POST['password'],
+      'email'=>$_POST['email']
+    );
+    
+    echo json_encode($parametros);
+  }
+  
 ?>
