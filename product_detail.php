e5bc6c87c8a550cd3cca90336c9e68928c125b5c
Dogmink <braulio25sc@gmail.com>
2020-12-22 17:36:30 -0500

    probando detalle del producto.php

--- product_detail.php
+++ product_detail.php
@@ -14,17 +14,20 @@ if(isset($_GET['id']) && is_numeric($_GET['id'])){
         <div class="contenido">
             <div class="row">
                 <div class="col6">
-                <?php
-                        $img = 'images/'.$value['img'];
+                    <?php
+                        $img = 'images/'.$result['img'];
                         if(file_exists($img)){
-                      ?>
+                    ?>
                         <img src="<?php print $img; ?>" width="90%">
                     <?php }else{?>
                         SIN FOTO
-                    <?php }?>
+                    <?php } ?>
                 </div>
                 <div class="col6">
-
+                    <h1><?php print $result['name'] ?></h1>
+                    <p><?php print $result['design_desc'] ?></p>
+                    <p><?php print 'S/.'.$result['precio']?></p>
+                    <a href="shopping-cart.php">Agregar al carrito</a>
                 </div>
             </div>
         </div>
