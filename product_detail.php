34bb8ae28b276d955090db7aff2259f1014f6ce2
Dogmink <braulio25sc@gmail.com>
2020-12-22 19:25:52 -0500

    Mejorando aspecto de product-detail.php

--- product_detail.php
+++ product_detail.php
@@ -12,6 +12,7 @@ if(isset($_GET['id']) && is_numeric($_GET['id'])){
         ?>
 
         <div class="contenido">
+                <h1 class="nameProductDetail"><?php print $result['name'] ?></h1>
             <div class="row">
                 <div class="col6">
                     <?php
@@ -24,8 +25,7 @@ if(isset($_GET['id']) && is_numeric($_GET['id'])){
                     <?php } ?>
                 </div>
                 <div class="col6">
-                    <h1 class="nameProductDetail"><?php print $result['name'] ?></h1>
-                    <p class="descProductDetail"><?php print $result['design_desc'] ?></p>
+                    <p class="descProductDetail">Descripción: <br> <?php print $result['design_desc'] ?></p>
                     <p class="priceProductDetail"><?php print 'S/.'.$result['precio']?></p>
                     <a class="btnProductDetail" href="shopping-cart.php">Agregar al carrito</a>
                 </div>
