fd853c30ad68e3b4d92dcef1dd29891dfecd928d
Dogmink <braulio25sc@gmail.com>
2021-02-19 13:19:08 -0500

    Fixed Productos Panel

--- panel/designs/index.php
+++ panel/designs/index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang='es'>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="../../css/normalize.css">
-  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../../css/grid.css">
-  <link rel="stylesheet" href="../../css/anim.css">
-  <link rel="stylesheet" href="../../css/estilos.css">
-  <link rel="stylesheet" href="../../css/mobile.css">
-  <link rel="stylesheet" href="../../css/bootstrap.min.css">
-  <title>BlackEdge</title>
-</head>
-<body>
-  <nav class="fadeInDown">
-    <div class="BlackEdge_image">
-      <img class="logo" src="../../images/Logo/LogoL.png" alt="index.html">
-    </div>
-    </nav>
-
-      <!-- Aquí acaba el nav -->
-
+<?php
+  require '../../Modulos/basicPanel.php';
+?>
       <div class="container">
         <div class="row">
           <div class="col12">
@@ -47,7 +25,7 @@
                 </thead>
                 <tbody>
                     <?php
-                      require '../../vendor/autoload.php';
+                      require '../../resources/design.php';
                       $design = new BlackEdgeStore\Design;
                       $info_designs = $design->mostrar();
                       $cantidad = count($info_designs);
