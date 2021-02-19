b0507536c83894fae3d9c738ff43611b8a62a761
Dogmink <braulio25sc@gmail.com>
2021-02-18 19:40:39 -0500

    fixed url css

--- Modulos/basicPanel.php
+++ Modulos/basicPanel.php
@@ -11,14 +11,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link class='iconweb' rel="icon" type="image/png" href="/images/icon/IconWeb.png" />
-  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="../css/normalize.css">
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
     rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/grid.css">
-  <link rel="stylesheet" href="css/anim.css">
-  <link rel="stylesheet" href="css/estilos.css">
-  <link rel="stylesheet" href="css/mobile.css">
+  <link rel="stylesheet" href="../css/grid.css">
+  <link rel="stylesheet" href="../css/anim.css">
+  <link rel="stylesheet" href="../css/estilos.css">
+  <link rel="stylesheet" href="../css/mobile.css">
   <title>BlackEdge</title>
 </head>
 
