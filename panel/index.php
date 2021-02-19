9bc9248646f8395b5ead208ea37a96811c1550ce
Dogmink <braulio25sc@gmail.com>
2021-02-18 19:29:06 -0500

    test2

--- panel/index.php
+++ panel/index.php
@@ -1,8 +1,8 @@
 <?php
-
+  session_start();
   if ($_SESSION[user_log] != null) {
     $user = $_SESSION[user_log];
-    if ($user[admin] === 9) {
+    if ($user[admin] == 9) {
       echo 'Hola a todos, soy el admin';
       require '../Modulos/basicPanel.php';
     }else{
