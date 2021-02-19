4e73382b4ecf75be0ebdc389967dddb1d442260b
Dogmink <braulio25sc@gmail.com>
2021-02-18 23:33:37 -0500

    se quita FadeInLeft al svg Login/Register

--- Modulos/basic.php
+++ Modulos/basic.php
@@ -798,7 +798,7 @@
 
       <li class="nav-item">
 	 	 <a href="register.php" class="nav-link">
-          <svg class="FadeInLeft" aria-hidden="true" focusable="false" data-prefix="fad" role="img" version="1.1" id="Capa_1"
+          <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" version="1.1" id="Capa_1"
             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
             viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
             <g>
