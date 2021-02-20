4d0bc1c7bfb2a9c8a4c4f090be1bf08787d9f4d9
Dogmink <braulio25sc@gmail.com>
2021-02-20 11:49:54 -0500

    order js fixed

--- Modulos/footerPanel.php
+++ Modulos/footerPanel.php
@@ -9,8 +9,8 @@
     <h3>info@blackedgestore.com</h3>
   </div>
 </footer>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
   <script src="https://blackedgestore.com/js/jquery-3.5.1.min.js" charset="utf-8"></script>
   <script src="https://blackedgestore.com/js/functions.js" charset="utf-8"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
 </body>
 </html>
