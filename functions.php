5a65898c9a7216282e1d7ce155b2d9fc00b1cad9
Dogmink <braulio25sc@gmail.com>
2021-02-24 12:17:09 -0500

    fix icon 2

--- functions.php
+++ functions.php
@@ -125,7 +125,7 @@
         <div class="desc-product_shop">
           <p><?php print $divice; print $item['precio'] ?></p>
           <a href="product_detail.php?id=<?php print $item['id']?>"> 
-          <svg class="icon-shop" aria-hidden="true" focusable="false" data-prefix="fad" role="img" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	            viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
             <g>
             	<g>
